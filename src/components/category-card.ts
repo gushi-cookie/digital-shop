730771501a21244940eee086e3700e109dcf3fb6
gushi-cookie <gushi-cookie@protonmail.com>
2023-09-06 22:33:13 +0300

    Create "brief-catalog" section.

---
+++ src/components/category-card.ts
@@ -0,0 +1,30 @@
+
+export interface CategoryCardProps {
+    name: string;
+    listNumber: number;
+    imageSrc: string;
+    inStock: number;
+    link: string,
+};
+
+
+export default function createElement(props: CategoryCardProps): HTMLDivElement {
+    let markdown = `
+    <div class="category-card">
+        <a class="category-card__wrap" href="${props.link}">
+            <span class="category-card__number">${('0' + props.listNumber).slice(-2)}</span>
+            <img class="category-card__img" src="${props.imageSrc}">
+        
+            <div class="category-card__info">
+                <span class="category-card__stock">${props.inStock} товаров</span>
+                <br>
+                <span class="category-card__name">${props.name}</span>
+            </div>
+        </a>
+    </div>
+    `;
+
+    let el = document.createElement('div');
+    el.innerHTML = markdown;
+    return el.children[0] as HTMLDivElement;
+};
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,47 @@ import 'normalize.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick.js';
 import './styles/main.css';
+import CategoryCard, { CategoryCardProps } from './components/category-card';
+
+
+
+const categoriesData: CategoryCardProps[] = [
+    {
+        name: 'Ноутбуки',
+        inStock: 105,
+        listNumber: 1,
+        imageSrc: '/images/categories/laptop.png',
+        link: '#',
+    },
+    {
+        name: 'Персональные компьютеры',
+        inStock: 65,
+        listNumber: 2,
+        imageSrc: '/images/categories/desktop.png',
+        link: '#',
+    },
+    {
+        name: 'Серверы',
+        inStock: 13,
+        listNumber: 3,
+        imageSrc: '/images/categories/server.png',
+        link: '#',
+    },
+    {
+        name: 'Dell EMC',
+        inStock: 2345,
+        listNumber: 4,
+        imageSrc: '/images/categories/emc.png',
+        link: '#',
+    },
+    {
+        name: 'Запасные части к Dell EMC',
+        inStock: 195,
+        listNumber: 5,
+        imageSrc: '/images/categories/emc-spare.png',
+        link: '#',
+    },
+];
 
 
 
@@ -16,3 +57,12 @@ $('#main-carousel').slick({
     prevArrow: false as any,
     nextArrow: `<span class="main-carousel__next-arrow"></span>`,
 });
+
+
+
+const categoryCards = new Array<HTMLElement>();
+categoriesData.forEach(data => {
+    categoryCards.push(CategoryCard(data));
+});
+
+$('.brief-catalog').append(categoryCards);

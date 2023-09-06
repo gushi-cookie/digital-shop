bb87293bd051594f1a64525bf0cd92991c08d51a
gushi-cookie <926396z@mail.ru>
2023-09-06 20:18:15 +0300

    Created a new site's section "best products".

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,18 @@
+import $ from 'jquery';
 import 'normalize.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick.js';
 import './styles/main.css';
+
+
+
+$('#main-carousel').slick({
+    dots: true,
+    infinite: true,
+    fade: true,
+    speed: 800,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    prevArrow: false as any,
+    nextArrow: `<span class="main-carousel__next-arrow"></span>`,
+});
